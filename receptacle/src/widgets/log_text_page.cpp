92f2a06d121dce51408fe90becd84a9aa524e0d2
William Wedler <wwedler@riseup.net>
2014-03-21 23:31:26 -0400

    adding log_text_page to ui

--- receptacle/src/widgets/log_text_page.cpp
+++ receptacle/src/widgets/log_text_page.cpp
@@ -6,9 +6,10 @@ LogTextPage::LogTextPage(QWidget *parent) : QWidget(parent){
     this->setLayout(layout);
     //this->page_layout = new QVBoxLayout();
     this->text_area = new LogText(this);
-    this->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    //this->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 }
 
-void LogTextPage::appendText(QString str){
-    this->text_area->appendPlainText(str);
+void LogText::appendText(QString str){
+    return this->text_area()->appendPlainText(str);
 }
