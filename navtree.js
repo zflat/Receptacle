ab61bc89a0add6d589fe2daed6ada66bc33601df
William Wedler <wwedler@riseup.net>
2014-03-27 21:53:59 -0400

    updated mainpage

--- navtree.js
+++ navtree.js
@@ -1,24 +1,11 @@
 var NAVTREE =
 [
-  [ "Receptacle", "index.html", [
-    [ "Classes", null, [
-      [ "Class List", "annotated.html", "annotated" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ]
-    ] ]
-  ] ]
+  [ "Receptacle", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"about__dialog_8h_source.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- search/all_72.js
+++ search/all_72.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['receptacle',['Receptacle',['../index.html',1,'']]],
-  ['request_5fcompleted',['request_completed',['../classDispatcher.html#aa63ab2c22fa0a85801c403d5fdd84749',1,'Dispatcher']]]
+  ['receptacle',['Receptacle',['../index.html',1,'']]]
 ];
